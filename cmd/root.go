75ff9d4bec9ef9d32e71b3b14611839a2d723432
jsirianni <jsirianni@localhost.localdomain>
2020-03-18 23:46:06 -0400

    add terminal alert option and better validation

--- cmd/root.go
+++ cmd/root.go
@@ -6,9 +6,9 @@ import (
 	"os"
 
 	"github.com/jsirianni/zfsmon/alert/slack"
+	"github.com/jsirianni/zfsmon/alert/terminal"
 	"github.com/jsirianni/zfsmon/zfs"
 
-	multierror "github.com/hashicorp/go-multierror"
 	"github.com/spf13/cobra"
 )
 
@@ -23,7 +23,7 @@ var z zfs.Zfs
 
 var rootCmd = &cobra.Command{
 	Use:   "zfsmon",
-	Short: "zfs monitoring daemon",
+	Short: "zfs monitoring util",
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := z.ZFSMon(); err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
@@ -47,7 +47,7 @@ func init() {
 
 	// alert flags
 	rootCmd.PersistentFlags().BoolVar(&noAlert, "no-alert", false, "do not send alerts")
-	rootCmd.PersistentFlags().StringVar(&alertType, "alert-type", "slack", "alert system to use")
+	rootCmd.PersistentFlags().StringVar(&alertType, "alert-type", "", "alert system to use")
 
 	// slack alert type
 	rootCmd.PersistentFlags().StringVar(&slackChannel, "channel", "", "slack channel")
@@ -55,38 +55,83 @@ func init() {
 }
 
 func initConfig() {
-	if err := checkFlags(); err != nil {
+	if err := initFlags(); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 
-	z = zfs.Zfs{}
-	z.State.File = stateFile
-	z.JSONOutput = jsonFmt
-	z.AlertConfig.NoAlert = noAlert
-	z.Alert = slack.Slack{hookURL, slackChannel}
+	if err := z.Init(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
 
+func initFlags() error {
+	if err := initSate(); err != nil {
+		return err
+	}
+	if err := initOutput(); err != nil {
+		return err
+	}
+	if err := initAlert(); err != nil {
+		return err
+	}
+	if err := initHostname(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func initSate() error {
+	z.State.File = stateFile
+	return nil
+}
+
+func initOutput() error {
+	z.JSONOutput = jsonFmt
+	return nil
+}
+
+func initHostname() error {
 	var err error
 	z.Hostname, err = os.Hostname()
 	if err != nil {
 		z.Hostname = "could_not_detect_hostname_sorry"
+		fmt.Println("could not detect hostname")
 	}
+	return nil
 }
 
-func checkFlags() error {
-	var e error
-
-	if noAlert == true {
-		return e
+func initAlert() error {
+	if noAlert {
+		z.AlertConfig.NoAlert = noAlert
+		return nil
 	}
 
-	if len(slackChannel) == 0 {
-		e = multierror.Append(e, errors.New("You must pass a channel '--channel <channel_name>' unless '--no-alert' is specified"))
+	if alertType == "" {
+		return errors.New("--alert-type is not set")
 	}
-
-	if len(hookURL) == 0 {
-		e = multierror.Append(e, errors.New("You must pass a slack hook url '--url <hook url>' unless --no-alert' is specified"))
+	if alertType == "slack" {
+		return initSlack()
 	}
+	if alertType == "terminal" {
+		return initTerminal()
+	}
+	return errors.New("alert type not valid: " + alertType)
+}
 
-	return e
+func initSlack() error {
+	if hookURL == "" {
+		return errors.New("hook url must be set when alert type is 'slack'")
+	}
+	if slackChannel == "" {
+		return errors.New("slack channel must be set when alert type is 'slack'")
+	}
+	z.Alert = slack.Slack{hookURL, slackChannel}
+	return nil
+}
+
+func initTerminal() error {
+	z.Alert = terminal.Terminal{}
+	return nil
 }
